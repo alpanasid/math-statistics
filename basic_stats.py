4d667e0455b5c90aeaf6acbec9d55a9dc98342e7
alpanasid <alpanasid@gmail.com>
2024-08-05 21:51:27 +0530

    Update basic_stats.py
    sorted given numbers and corrected the median logic

--- basic_stats.py
+++ basic_stats.py
@@ -1,10 +1,23 @@
-my_numbers = [2,4,6,8,10]
+my_numbers = [155, 158, 159, 161, 160, 162, 165]
+
+heights_of_studs = sorted(my_numbers)
 print("The given numbers are", my_numbers)
+print ("The sorted numbers are", heights_of_studs)
 sum_of_obs = 0
 
-for num in my_numbers:
+for num in heights_of_studs:
     sum_of_obs = sum_of_obs + num 
 
-mean = sum_of_obs / len(my_numbers)
+mean = sum_of_obs / len(heights_of_studs)
 
 print("The mean of given numbers is", mean)
+
+length_num = len(heights_of_studs)
+
+
+if length_num % 2 == 0:
+    median = (length_num//2) + ((length_num + 1)/2)
+if length_num % 2 != 0:
+    median = ((length_num +1)/2)
+
+print("The median of the given numbers is" ,  heights_of_studs[int(median-1)])
